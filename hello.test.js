7ec3539539e929e4ae61b8008e6cbbfd84ed9348
ignat-satir <195108257+ignat-satir@users.noreply.github.com>
2025-07-09 18:42:30 +0300

    fix: remove invalid JSON from test file

--- hello.test.js
+++ hello.test.js
@@ -2,7 +2,3 @@
 it('always passes', () => {
   expect(true).toBe(true);
 });
-
-"scripts": {
-  "test": "jest --reporters=default --reporters=\"@shelex/jest-allure2-reporter\" --outputDirectory=allure-results"
-}
